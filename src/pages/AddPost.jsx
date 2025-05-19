6b2ce3bccc31cfe72430b4a5d5c723bbfe85e128
DS85300 <sagan@parkstern.de>
2025-05-19 14:38:56 +0200

    beginningformDataIntegration

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -7,6 +7,7 @@ const AddPost = () => {
 const [formData,setFormData] = useState({
     photos: [],
     title: '',
+    category:'',
     description:'',
     collectionTime:'',
     location:'',
@@ -21,8 +22,9 @@ const handleChange = (e) => {
     const {name,value,files} = e.target;
 
     if(name==='photos'){
-        const selectedFiles = Array.from(files)
+        const selectedFiles = Array.from(files) //creating array from uploaded img, each img is an element in the array
 
+        //upload limit = 5 photos
         if(formData.photos.length + selectedFiles.length > 5) {
             setError('Max 5 photos possible');
             return
@@ -31,12 +33,12 @@ const handleChange = (e) => {
 
         const newUrls = selectedFiles.map((file)=> URL.createObjectURL(file));
         
-    
+    //adding photos to form data
     setFormData((prev)=>({
         ...prev,
         photos: [...prev.photos,...selectedFiles]
     }));
-    setPreviewUrls(prev => [...prev, ...newUrls]);
+    setPreviewUrls(prev => [...prev, ...newUrls]);//showing existing and new photos in preview
 } else {
     setFormData((prev)=> ({
         ...prev,
@@ -45,8 +47,24 @@ const handleChange = (e) => {
 }
 };
 
-const handleSubmit = () => {
-    console.log(formData.title)
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = new FormData();
+
+    formData.photos.forEach((photo)=>{
+        data.append('photo',photo);
+    });
+    data.append('title',formData.title);
+    data.append('categrory',formData.category);
+    data.append('description',formData.description);
+    data.append('collecionTime', formData.collectionTime);
+    data.append('location', formData.location);
+   
+    for (let pair of data.entries()){
+        console.log(pair[0], pair[1]);
+    }
+    
 }
 
 
@@ -90,6 +108,28 @@ const handleSubmit = () => {
                 onChange={handleChange}
                 required
                  />
+            </div>
+            <div className="category-div">
+                <label className="category-label" htmlFor="title"></label>
+                <select 
+                id="category"
+                name="category"
+                value={formData.category}
+                onChange={handleChange}
+                required
+                 >
+                    <option value="">All Cagegories</option>
+                    <option value="">Furniture/Household</option>
+                    <option value="">Electronics</option>
+                    <option value="">Books</option>
+                    <option value="">Services</option>
+                    <option value="">Tickets</option>
+                    <option value="">Hobby</option>
+                    <option value="">For Kids</option>
+                    <option value="">Clothes</option>
+                    <option value="">Materials</option>
+                    <option value="">Else</option>
+                 </select>
             </div>
             <div className="description-div">
                 <label className="description-label" htmlFor="description">Description</label>
