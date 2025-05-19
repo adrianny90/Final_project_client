d11b4e0bc928f1e53ab92814371d2f0b1bc42c9d
Manoj Maharjan <manoj.maharjan@hotmail.com>
2025-05-19 08:55:45 +0200

    install packages, add layouts for Homepage

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+
+const MainLayout = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow container mx-auto px-4 py-8">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
