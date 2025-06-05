710e73a13fa5f4e077e0e9f7d2e6c1bbd110f94a
Manoj Maharjan <manoj.maharjan@hotmail.com>
2025-06-05 23:51:31 +0200

    filter items by post type and categories

--- src/components/CategorySelect.jsx
+++ src/components/CategorySelect.jsx
@@ -1,4 +1,6 @@
-const CategorySelect = ({ value, onChange,className=''}) => (
+import categories from "../utils/categories.js";
+
+const CategorySelect = ({ value, onChange, className = "" }) => (
   <div className={`mb-6 ${className}`}>
     <label htmlFor="category" className="block text-black">
       Category
@@ -11,27 +13,14 @@ const CategorySelect = ({ value, onChange,className=''}) => (
       required
       className="w-full border border-black rounded-md p-2 text-gray-700"
     >
-      <option value="">All Categories</option>
-      <option value="Books">Books</option>
-      <option value="Baby and Toys">Baby and Toys</option>
-      <option value="Beauty and Personal Care">Beauty and Personal Care</option>
-      <option value="Clothing and Accessories">Clothing and Accessories</option>
-      <option value="Electronics">Electronics</option>
-      <option value="Furniture">Furniture</option>
-      <option value="Garden Accessories and Plants">
-        Garden Accessories and Plants
-      </option>
-      <option value="Household Appliances and Goods">
-        Household Appliances and Goods
-      </option>
-      <option value="Kitchen Appliances and Accessories">
-        Kitchen Appliances and Accessories
-      </option>
-      <option value="Hobby">Hobby</option>
-      <option value="Sports and Outdoors">Sports and Outdoors</option>
-      <option value="Pet Supplies">Pet Supplies</option>
-      <option value="Services">Services</option>
-      <option value="Others">Others</option>
+      {categories.map((category) => (
+        <option
+          key={category}
+          value={category === "All Categories" ? "" : category}
+        >
+          {category}
+        </option>
+      ))}
     </select>
   </div>
 );
