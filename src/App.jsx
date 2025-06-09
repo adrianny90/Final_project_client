5b388457cd0f9bda5e2168c8f9a6f810b0d873fb
Adrian B <adrianny90@wp.pl>
2025-06-09 19:13:57 +0200

    implement google

--- src/App.jsx
+++ src/App.jsx
@@ -15,38 +15,41 @@ import Panel from "./pages/Panel.jsx";
 import Terms from "./pages/Terms.jsx";
 import Privacy from "./pages/Privacy.jsx";
 import About from "./pages/About.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Register />} />
-          <Route path="/verify/:token" element={<Verify />} />
+    <GoogleOAuthProvider clientId={`${import.meta.env.VITE_GOOGLE_CLIENT_ID}`}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Register />} />
+            <Route path="/verify/:token" element={<Verify />} />
 
-          <Route
-            element={
-              <AuthContextProvider>
-                <ProtectedLayout />
-              </AuthContextProvider>
-            }
-          >
-            <Route path="/give" element={<AddPost />} />
-            <Route path="/panel" element={<Panel />} />
+            <Route
+              element={
+                <AuthContextProvider>
+                  <ProtectedLayout />
+                </AuthContextProvider>
+              }
+            >
+              <Route path="/give" element={<AddPost />} />
+              <Route path="/panel" element={<Panel />} />
+            </Route>
+
+            <Route path="/get" element={<GetFreePage />} />
+            <Route path="/map" element={<ItemMap />} />
+            <Route path="get/items/:id" element={<ItemDetails />} />
+            <Route path="/terms" element={<Terms />} />
+            <Route path="/privacy" element={<Privacy />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
           </Route>
-
-          <Route path="/get" element={<GetFreePage />} />
-          <Route path="/map" element={<ItemMap />} />
-          <Route path="get/items/:id" element={<ItemDetails />} />
-          <Route path="/terms" element={<Terms />} />
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>
+    </GoogleOAuthProvider>
   );
 }
 
